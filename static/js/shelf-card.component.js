f7e4582117c7cb6046f1d38652a18e2067086428
tungdm <tungdm@sid.localdomain>
2018-12-25 16:45:23 +0700

    - add table of content

--- static/js/shelf-card.component.js
+++ static/js/shelf-card.component.js
@@ -6,10 +6,21 @@ Vue.component('shelf-card', {
   },
   mounted: function() {
     this.fetchBooksInfo();
+    /*
+    $(".sliding-link").on('click',function(e) {
+      this.slideLink(e);
+    });
+    */
   },
-  computed: {
+  beforeDestroy(){
+    //$('.sliding-link').off('click', function (e) {});
   },
   methods: {
+    slideLink: function(tag) {
+      console.log("tag:",tag);
+      var aid = $(".card[jumptag='"+tag+"']");
+      $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+    },
     loadBook: function(book) {
       var self = this;
       console.log('inside shelf-comp.loadBook:',book)
@@ -43,16 +54,36 @@ Vue.component('shelf-card', {
     }
   },
   filters: {
+    firstChar: function(s) {
+      return s.toUpperCase()[0];
+    }
   },
-  template: `<div class="main-template">              
+  computed: {
+    azIndex: function() {
+      var idx = [];
+      if (this.books) {
+        var bL = this.books.length;
+        for (var i=0;i<bL;i++) {
+          var fC = this.books[i].toUpperCase()[0];
+          if (idx.indexOf(fC) === -1) idx.push(fC);
+        }
+        return idx;  
+      }
+      return [];
+    }
+  },
+  template: `<div class="main-template">
+              <div class="row" id="tableOfContent">
+                <a href="#" class="sliding-link" v-for="(idx) in azIndex" v-bind:jumptag="idx" @click="slideLink(idx)">{{idx}}</a>
+              </div>
               <div class="row">
               <div class="col-sm-4" v-for="(item) in books"  >
-              <div class="card"  >
+              <div class="card" v-bind:jumptag="item | firstChar" >
                 <div class="card-title card-header text-monospace text-truncate" >
-                  <a v-bind:href="'#'+item" @click="loadBook(item)">{{item}}</a>
+                <a href="#tableOfContent" class="link-top"><i data-feather="arrow-up-circle"></i></a><a v-bind:id="item">{{item}}</a>
                 </div>
                 <div class="card-body">
-                <a v-bind:href="'#'+item" @click="loadBook(item)"><img class="card-img-bottom" :src="'/pages/'+item+'/'+item+'.thumb'"/></a>
+                <a href="#" @click="loadBook(item)"><img class="card-img-bottom" :src="'/pages/'+item+'/'+item+'.thumb'"/></a>
                 </div>
               </div>
               </div>
